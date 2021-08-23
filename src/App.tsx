1e9122888e8af03ac4db24fe2eff350f9b229181
Tamiris <tamiris-adm@hotmail.com>
2021-08-23 19:16:44 -0300

    React

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Router from "./routes";
+import GlobalStyle from "./styles/globalStyle";
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return(
+    <>
+      <ToastContainer />
+      <GlobalStyle />
+      <Router />
+    </>
+  )
+}
+
+export default App;
